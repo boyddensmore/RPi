01429bf8e25e174d12b9b6125afe7af55ba18a98
boyddensmore <boyddensmore@protonmail.com>
2016-03-19 14:45:13 -0600

    added phonecontrol.c

---
+++ phonecontrol.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+#define uchar unsigned char
+  
+#define DialPin	 25
+#define HookPin	 25
+
+
+int main(void)
+{
+
+	//~ pinMode(LedPin, OUTPUT);
+	pinMode(DialPin, INPUT);
+	pinMode(HookPin, INPUT);
+	
+	while(1){
+
+		}
+	}
+	
+
+	return 0;
+}
