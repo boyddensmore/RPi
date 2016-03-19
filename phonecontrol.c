a5ec77dc82e3880cb58e61b70732787669e3df9d
boyddensmore <boyddensmore@protonmail.com>
2016-03-19 15:58:50 -0600

    Added On Hook, Dialing, and Dial Count

--- phonecontrol.c
+++ phonecontrol.c
@@ -1,22 +1,50 @@
+#include <wiringPi.h>
 #include <stdio.h>
 
 
 #define uchar unsigned char
   
-#define DialPin	 25
-#define HookPin	 25
+#define HookPin	 	29
+#define DialPin	 	25
+#define DialingPin	24
 
 
 int main(void)
 {
+	if(wiringPiSetup() == -1){ //when initialize wiring failed, print message to screen
+		printf("Setup wiringPi failed!");
+		return 1;
+	}
 
-	//~ pinMode(LedPin, OUTPUT);
+	int dropCount = 0;
+
 	pinMode(DialPin, INPUT);
+	pinMode(DialingPin, INPUT);
 	pinMode(HookPin, INPUT);
 	
+	pullUpDnControl(DialPin, PUD_UP);
+	pullUpDnControl(DialingPin, PUD_UP);
+	pullUpDnControl(HookPin, PUD_UP);
+	
 	while(1){
-
+		while(digitalRead(HookPin) == 0){
+			printf("Off Hook\n");
+			while (digitalRead(DialingPin) == 0) {
+				printf("Dialing\n");
+				while (digitalRead(DialPin) == 1 && digitalRead(HookPin) == 0) {
+					delay(1);
+				}
+				while (digitalRead(DialPin) == 0 && digitalRead(HookPin) == 0) {
+					delay(1);
+				}
+				dropCount ++;
+			}
+			printf("dropCount: %d\n", dropCount);
 		}
+		printf("On Hook\n");
+		dropCount = 0;
+		
+		//~ printf("DR DialingPin: %d\n", digitalRead(DialingPin));
 	}
 	
 
