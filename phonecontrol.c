264a5e0dd41779f65128e9892314c7f069498123
boyddensmore <boyddensmore@protonmail.com>
2016-03-20 00:23:05 -0600

    Generates 11 digit numbers and "dials"

--- phonecontrol.c
+++ phonecontrol.c
@@ -17,6 +17,11 @@ int main(void)
 	}
 
 	int dropCount = 0;
+	int dialedNumberLength = 11;
+	int dialedNumber [dialedNumberLength];
+	int dialNumberIndex = 0;
+	int dialConfirmation = 0;
+	int x = 0;
 
 	pinMode(DialPin, INPUT);
 	pinMode(DialingPin, INPUT);
@@ -27,26 +32,70 @@ int main(void)
 	pullUpDnControl(HookPin, PUD_UP);
 	
 	while(1){
-		while(digitalRead(HookPin) == 0){
-			printf("Off Hook\n");
+		while(digitalRead(HookPin) == 1){
+		}
+		
+		//~ printf("HookPin: %d\n", digitalRead(HookPin));
+		printf("Off Hook.\n");
+	
+		while(digitalRead(HookPin) == 0 && dialNumberIndex < dialedNumberLength){
+
+			while (digitalRead(DialingPin) == 1 && digitalRead(HookPin) == 0) {
+			}
+			
 			while (digitalRead(DialingPin) == 0) {
-				printf("Dialing\n");
-				while (digitalRead(DialPin) == 1 && digitalRead(HookPin) == 0) {
+				//~ printf("DialingPin: %d\n", digitalRead(DialingPin));
+				//~ printf("dropCount: %d\n", dropCount);
+				while (digitalRead(DialPin) == 1 && digitalRead(HookPin) == 0 && digitalRead(DialingPin) == 0) {
 					delay(1);
+					dialConfirmation = 1;
 				}
-				while (digitalRead(DialPin) == 0 && digitalRead(HookPin) == 0) {
+				while (digitalRead(DialPin) == 0 && digitalRead(HookPin) == 0 && digitalRead(DialingPin) == 0) {
 					delay(1);
+					dialConfirmation = 2;
 				}
 				dropCount ++;
 			}
-			printf("dropCount: %d\n", dropCount);
+			
+			if (dialConfirmation == 2) {
+			
+				dropCount = (dropCount - 1) % 10;
+				
+				//~ printf("dropCount: %d\n", dropCount);
+				dialedNumber[dialNumberIndex] = dropCount;
+				printf("Dialed Number: ");
+				for (x = 0; x <= dialNumberIndex; x++) {
+					printf("%d", dialedNumber[x]);
+				}
+				printf("\n");
+				
+				dialNumberIndex ++;
+			}
+			else {
+				printf("----------Bad Dial----------\n");
+			}
+
+			dropCount = 0;
+			dialConfirmation = 0;
 		}
-		printf("On Hook\n");
-		dropCount = 0;
 		
-		//~ printf("DR DialingPin: %d\n", digitalRead(DialingPin));
+		if (dialNumberIndex >= dialedNumberLength) {
+			printf("...Dialing ");
+			for (x = 0; x < dialedNumberLength; x++) {
+				printf("%d", dialedNumber[x]);
+			}
+			printf("...\n");
+		}
+		
+		if (digitalRead(HookPin) == 1){
+			printf("Hung Up.\n");
+		}
+		
+		
+		dropCount = 0;
+		dialNumberIndex = 0;
+		dialConfirmation = 0;
 	}
-	
 
 	return 0;
 }
